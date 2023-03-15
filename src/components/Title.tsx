38d7ade2347a2c91cb490c2df9343e6ff09068a8
rhesus2016 <rhesus2016@gmail.com>
2023-03-15 23:30:50 +0900

    feature: 컴포넌트 타이틀 구현

---
+++ src/components/Title.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+interface Props {
+  title: string;
+}
+
+const Title = (props: Props) => {
+  const { title } = props;
+
+  return <TitleStyle>{title}</TitleStyle>;
+};
+
+export default Title;
+
+const TitleStyle = styled.div`
+  font-weight: 700;
+  font-size: 22px;
+  color: rgba(42, 50, 73, 1);
+  margin-bottom: 24px;
+`;
